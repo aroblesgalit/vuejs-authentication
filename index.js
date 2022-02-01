187064f16199f4037f23c9079de9b7fce4a6fe9f
Alvin Galit <aroblesgalit@gmail.com>
2022-01-31 19:44:20 -0700

    Added comments for each line of code in index file

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 
+// Initialize app with express
 app = express()
 
+// Route
 app.get('/', (req, res) => {
   res.send('Hello')
 })
 
+// Port
 const port = process.envPORT || 5000
 
+// Start server
 app.listen(port, () => console.log(`Server started on port ${port}`))
