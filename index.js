b89ae5e64df948a027dec08d73a5e5496d76de10
Alvin Galit <aroblesgalit@gmail.com>
2022-02-01 19:02:52 -0700

    Created connection to database

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const mongodb = require('mongodb')
+require('dotenv').config()
+
+// Connect to database
+async function loadDatabase () {
+  const client = await mongodb.MongoClient.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true
+  })
+  return client.db('auth')
+}
 
 // Initialize app with express
 app = express()
 
 // Route
-app.get('/', (req, res) => {
-  res.send('Hello')
+app.get('/', async (req, res) => {
+  const data = await loadDatabase()
+  res.send(await data.find({}).toArray())
 })
 
 // Port
