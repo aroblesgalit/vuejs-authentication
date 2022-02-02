6c6f1649d9098fa6fc8235a913f51e82214895c0
Alvin Galit <aroblesgalit@gmail.com>
2022-02-01 19:47:16 -0700

    Used bcrypt to has password

--- model/user.js
+++ model/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
+const SALT_WORK_FACTOR = 10
 
 const userSchema = new Schema({
   username: {
@@ -18,6 +20,26 @@ const userSchema = new Schema({
     ]
   }
 })
+
+userSchema.pre('save', async function save (next) {
+  // Only hash the password if it has been modified (or is new)
+  if (!this.isModified('password')) {
+    return next()
+  }
+  try {
+    // Generate salt
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR)
+    // Hash the password along with the new salt
+    this.password = await bcrypt.hash(this.password, salt)
+    return next()
+  } catch (err) {
+    return next(err)
+  }
+})
+
+userSchema.methods.validatePassword = async function validatePassword (data) {
+  return bcrypt.compare(data, this.password)
+}
 
 const User = mongoose.model('User', userSchema)
 
