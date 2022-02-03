da32b8b2e07851dbd1c3a558782be7e50a7b02d7
Alvin Galit <aroblesgalit@gmail.com>
2022-02-02 19:21:37 -0700

    Passed in props credentials and set to true within cors

--- index.js
+++ index.js
@@ -2,20 +2,6 @@ const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const cors = require('cors')
-// const User = require('./model/user')
-
-// Initialize app with express
-app = express()
-
-app.use(express.json())
-
-// Middle ware
-app.use(cors())
-
-const users = require('./routes/api/users')
-app.use('/api/users', users)
-
-const port = process.env.PORT || 5000
 
 // Connect to database
 mongoose.connect(
@@ -28,6 +14,18 @@ mongoose.connect(
     console.log('MongoDB database connection established successfully.')
   }
 )
+
+// Initialize app with express
+app = express()
+app.use(express.json())
+
+// Middle ware
+app.use(cors({ credentials: true }))
+
+const users = require('./routes/api/users')
+app.use('/api/users', users)
+
+const port = process.env.PORT || 5000
 
 // Start server
 app.listen(port, () => console.log(`Server started on port ${port}`))
