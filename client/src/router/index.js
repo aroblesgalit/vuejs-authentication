c0d98a8e84b2197f5372a59075ddb623637ed5fb
Alvin Galit <aroblesgalit@gmail.com>
2022-02-03 20:30:25 -0700

    Added routes for Home, Login, and Register pages

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
+import Register from '@/views/Register.vue'
 
-const routes = []
+const routes = [
+  { path: '', component: Home },
+  { path: '/login', component: Login },
+  { path: '/register', component: Register }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
